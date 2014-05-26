retrotinker/bcc09

commit 060967469da19bf8dffc60198b921a9aac64a8fc

Remove bulk of non-6809 source code w/ unifdef
	unifdef -UI8086 -UI8088 -UI80386 -U__AS386_16__ -U__AS386_32__ \
		-UMSDOS -UEDOS -USOS_MSDOS -USOS_EDOS -UTOS_EDOS -DMC6809

Signed-off-by: John W. Linville <linville@tuxdriver.com>

ld/obj.h
@@ -5,17 +5,9 @@
 #define OBJ_H
 
 #ifndef OMAGIC
-# ifdef I80386
-#  define OMAGIC 0x86A3
-# endif
 
-# ifdef I8086
-#  define OMAGIC 0x86A0
-# endif
 
-# ifdef MC6809
 #  define OMAGIC 0x5331
-# endif
 #endif
 
 #ifdef LONG_OFFSETS
@@ -26,12 +18,7 @@
 # define offtocn u2cn
 #endif
 
-#ifdef MC6809			/* temp don't support alignment at all */
 # define ld_roundup( num, boundary, type ) (num)
-#else
-# define ld_roundup( num, boundary, type ) \
-	(((num) + ((boundary) - 1)) & (type) ~((boundary) - 1))
-#endif
 
 #define MAX_OFFSET_SIZE 4
 #define NSEG 16

commit aea7a9f3344459fc81d830b82af6b2bac12139ab

ld: correct the implementation of ld_roundup
This is fallout from commit 060967469da.  I'm not sure why the -DMC6809
version was a NOP in the first place, but it is definitely wrong for
the current state of the project.

Signed-off-by: John W. Linville <linville@tuxdriver.com>

ld/obj.h
@@ -18,7 +18,8 @@
 # define offtocn u2cn
 #endif
 
-# define ld_roundup( num, boundary, type ) (num)
+# define ld_roundup( num, boundary, type ) \
+	(((num) + ((boundary) - 1)) & (type) ~((boundary) - 1))
 
 #define MAX_OFFSET_SIZE 4
 #define NSEG 16